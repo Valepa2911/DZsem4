45a654e070b30c222b24696f2728543e34e91343
Valepa2911 <megp17102017@mail.ru>
2022-12-08 10:48:59 +0400

    сохран2.

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] mass = new int[8];
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(0,9);
+
+    Console.Write(" " + mass[i]);
+}
